dab2a56fef28004ed38b0526c01e70ed7338f95c
Lewislusweti <144348219+Lewislusweti@users.noreply.github.com>
2025-08-05 22:30:05 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;CRECDbZK#V8B0zQ82E46yS8DcVIeKrOt8_JE-he3p7O5h8s1usiA", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;LIMl0YLI#wWtI6tUX4e5G_Ml-ma545-nxsVZmfVbyEH39GeURauo", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "254741749876", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
