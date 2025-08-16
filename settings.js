6bd64af4aa65d765e27b0693d8cf5a13773124a7
Lewislusweti <144348219+Lewislusweti@users.noreply.github.com>
2025-08-16 22:52:45 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;GZM3GJbB#y8CxnJoXoJvidPDA2JkWHFn3lXF-B1HMhtKDrOGDYrA", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;LE9EAQIQ#tZiQLMTEUHk_9cjUheDPXf8nnlY1DLtqoVdkjtR1Dy4", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "254741749876", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
